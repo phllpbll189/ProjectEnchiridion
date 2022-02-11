cfe94337f43671962bdd40b25ce60b5ffe9ad085
Phillip Bell <phillip.bell@mymail.champlain.edu>
2022-02-11 16:49:30 -0500

    debugging bug Error: Can't resolve '../aws-exports'

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {HomePage, Editor, SignUp} from './routes';
 import { AmplifyProvider } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import Amplify from 'aws-amplify';
+import awsexports from './aws-exports'
+Amplify.configure(awsexports);
 //this is what is called to render the react element initially.
 ReactDOM.render(
   <AmplifyProvider>
--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -4,9 +4,6 @@ import '@aws-amplify/ui-react/styles.css';
 import '../CSS/Signup/SignUp.css';
 import TopBar from '../components/Homepage/TopBar';
 import Profile from '../components/Signup/Profile'
-import Amplify from 'aws-amplify';
-import awsmobile from '../aws-exports'
-Amplify.configure(awsmobile);
 
 //this is what is called to render the react element initially.
 function SignUp(){
