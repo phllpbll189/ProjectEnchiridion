9f57a766fd4e8123c01824c05ebba700d0fd645f
Phillip Bell <phillip.bell@mymail.champlain.edu>
2022-02-14 07:33:09 -0500

    added the start to drag and drop. need to clean this up.

--- src/routes/Editor.jsx
+++ src/routes/Editor.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import TopBar from '../components/Homepage/TopBar';
-import ComponentBar from '../components/Editor/ComponentBar.jsx';
+import LeftBar from '../components/Editor/LeftBar';
+import EditBox from '../components/Editor/EditBox';
 import '../CSS/Editor/Editor.css';
 
-
 export default function Editor(){
-    // TODO make constructor and store props from state that is passed down
+    const sideStyle = {
+        background: "#999999",
+        paddingLeft: "5px",
+    }
+    // going to need to save the divs as arrays within arrays. each of which will have a short description that can be used to generate react code.
+    // TypeScript will be usefule to make our own classes and inheriting a common function to create jsx. <div className={} id={} draggable="true" onDragStart={} onDrop={} onDragOver={event.preventDefault()} onDragEnd={}>
     return(
         <>
             <TopBar/>
             <div className='column_container'>
-                <div className='editor_left' style={{background: "#000000"}}>hello</div>
-                <div className='main_divider'  style={{background: "#ffffff"}}>world</div>
+                <LeftBar></LeftBar>
+                <EditBox></EditBox>
             </div>
         </>
     );
